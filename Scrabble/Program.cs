bdaad9e872cd7a89d27a4884ec6d1a26659fffc5
Rachel Schieferstein and Todd Walraven <rachel@>
2020-03-05 12:00:09 -0800

    add EndState method for better user interface

--- Scrabble/Program.cs
+++ Scrabble/Program.cs
@@ -8,14 +8,31 @@ namespace Program
   {
     public static void Main()
     {
-        Console.WriteLine("Enter a word:");
+      Console.WriteLine("Enter a word:");
 
-        string response = Console.ReadLine();
+      string response = Console.ReadLine();
 
-        Word newWord = new Word(response);
-
-        // newWord.Score();
-        Console.WriteLine(newWord.Score(response));
+      Word newWord = new Word(response);
+      string lower =newWord.MakeLower(response);
+      Console.WriteLine("the score of your word is : " + newWord.Score(lower) + " points!");
+      EndState();
     }
+    public static void EndState()
+        {
+          Console.WriteLine("would you like to try another word? [y] yes [n] no");
+          string playAgain = Console.ReadLine();
+          if(playAgain == "y")
+          {
+            Main();
+          }
+          else if (playAgain == "n")
+          {
+            Console.WriteLine("goodbye");
+          }
+          else
+          {
+            EndState();
+          }
+        }
   }
 }
